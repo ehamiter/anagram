562500973bd9e4f7ace6ed57c09f12247a864ee3
Raphael Eidus <raphaeleidus@users.noreply.github.com>
2015-09-09 04:05:00 -0400

    change the required flag to require letters in any order or position

--- anagram.py
+++ anagram.py
@@ -1,6 +1,5 @@
 import argparse
 
-
 def anagramchk(word,chkword):
     for letter in word:
         if letter in chkword:
@@ -9,44 +8,62 @@ def anagramchk(word,chkword):
             return 0
     return 1
 
+def hasAllRequired(word,required):
+    for letter in required:
+        if letter not in word:
+            return False
+        word = word.replace(letter, '', 1)
+    return True
+
+def solve(opts):
+    f=open('wordlist.txt', 'r')
+    words = []
+
+    for line in f:
+        line=line.strip()
+
+        if anagramchk(line,opts['letters']):
+            words.append(line)
+
+            if opts['min']:
+                for word in words:
+                    if not len(word)>=int(opts['min']):
+                        words.remove(word)
+                
+            if opts['starts']:
+                for word in words:
+                    if word[0] != opts['starts']:
+                        words.remove(word)
+            
+            if opts['ends']:
+                for word in words:
+                    if word[-1] != opts['ends']:
+                        words.remove(word)
+
+            if opts['required']:
+                for word in words:
+                    if not hasAllRequired(word, opts['required']):
+                        words.remove(word)
+
+            if not opts['unordered']:
+                words.sort(key=len, reverse=True)
+
+            if opts['limit']:
+                words=words[:int(opts['limit'])]
+        
+    for word in words:
+        print(word)
+
+    f.close()
+
 parser = argparse.ArgumentParser(description='Anagram generator with several options to control the generated words')
 parser.add_argument('-l','--letters', help='Letters provided to generate an anagram', required=True)
 parser.add_argument('-m','--min', help='Minimum amount of letters in anagram', required=False)
 parser.add_argument('-s','--starts', help='Generates words starting with this letter', required=False)
 parser.add_argument('-e','--ends', help='Generates words ending with this letter', required=False)
 parser.add_argument('-r','--required', help='Generates words that must have this letter or letters', required=False)
+parser.add_argument('-u', '--unordered', action='store_true', help='Leave the output in alphabetical order', required=False)
+parser.add_argument('--limit', help='The maximum number of words you want to see', default=5, required=False)
 args = vars(parser.parse_args())
 
-f=open('wordlist.txt', 'r')
-words = []
-
-for line in f:
-    line=line.strip()
-
-    if anagramchk(line,args['letters']):
-        words.append(line)
-
-        if args['min']:
-            for word in words:
-                if not len(word)>=int(args['min']):
-                    words.remove(word)
-            
-        if args['starts']:
-            for word in words:
-                if word[0] != args['starts']:
-                    words.remove(word)
-        
-        if args['ends']:
-            for word in words:
-                if word[-1] != args['ends']:
-                    words.remove(word)
-
-        if args['required']:
-            for word in words:
-                if args['required'] not in word:
-                    words.remove(word)
-    
-for word in words:
-    print word
-
-f.close()
+solve(args)
